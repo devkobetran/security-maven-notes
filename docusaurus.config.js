0c5931c4a0ab71dc46c215c78d228d226dd0e511
devkobetran <kobetrankhoi222@gmail.com>
2024-12-05 22:36:34 -0600

    fix: new notes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "https://devkobetran.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/security-notes/",
+  baseUrl: "/security-maven-notes/",
 
   // Add trailingSlash configuration
   trailingSlash: false,
